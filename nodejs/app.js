9a6cdf8fc9556ee90151d50da80ad0b74f81fb29
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-09 16:34:21 +0200

    added module export to main.js

---
+++ nodejs/app.js
@@ -0,0 +1,6 @@
+const userProfile = require('./main.js');
+// users.forEach(username => getProfile(username));
+//when the function take one parameter and foreach passes one in we can shorten our code to :
+// there is another way to use global object process
+const users = process.argv.slice(2);
+users.forEach(userProfile.getProfile);
--- nodejs/main.js
+++ nodejs/main.js
@@ -1,15 +1,16 @@
-//  const users =["chalkers",  , ];
+//  const users =["chalkers","davemcfarland"];
 function getProfile(username) {
-  const https = require("https");
-  const http = require("http");
-  const request = https.get(
-    `https://teamtreehouse.com/${username}.json`,
-    response => {
-      if (response.statusCode === 200) {
-        let responseBody = "";
-        response.on("data", data => {
-          responseBody += data.toString();
-        });
+  try{
+    const https = require("https");
+    const http = require("http");
+    const request = https.get(
+        `https://teamtreehouse.com/${username}.json`,
+        response => {
+        if (response.statusCode === 200) {
+            let responseBody = "";
+            response.on("data", data => {
+            responseBody += data.toString();
+            });
         response.on("end", () => {
           try {
             const profile = JSON.parse(responseBody);
@@ -18,20 +19,24 @@ function getProfile(username) {
             printError(error);
           }
         });
-      } else {
-        const message = `There was an error for ${username}${
-          http.STATUS_CODES[response.statusCode]
-        }`;
-        const statusCodeError = new Error(message);
-        printError(statusCodeError);
-      }
+        } else {
+            const message = `There was an error for ${username}${
+            http.STATUS_CODES[response.statusCode]
+            }`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
     }
   );
 
   request.on("error", error => {
     console.error(`error is : ${error.message}`);
-  });
-}
+    });
+}catch(error){
+    printError(error);
+}  
+} 
+
 function printError(error) {
   console.error(error.message);
 }
@@ -40,9 +45,4 @@ function dispalyData(data) {
     `${data.name} has ${data.points.total} and ${data.badges.length}`
   );
 }
-// users.forEach(username => getProfile(username));
-//when the function take one parameter and foreach passes one in we can shorten our code to :
-
-// there is another way to use global object process
-const users = process.argv.slice(2);
-users.forEach(getProfile);
+module.exports.getProfile = getProfile;
