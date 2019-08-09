c75027562421bf4566e1ffecef64ccdf570a9981
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-09 10:32:35 +0200

    added a new function to the homework

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,4 +1,27 @@
-const https = require("https");
-https.get('https://teamtreehouse.com/chalkers.json', response =>{
+
+const username =["chalkers", "alenaholligan","davemcfarland"];
+function getProfile(username){
+    const https = require("https");
+    https.get(`https://teamtreehouse.com/${username}.json`, response =>{
     console.log('statusCode', response.statusCode);
+    let responseBody = "";
+    response.on('data', data =>{
+        responseBody += data.toString();
+    });
+    response.on('end', ()=>{
+        // console.log(typeof(responseBody));
+        // console.log(responseBody);
+        const profile = JSON.parse(responseBody);
+    //    console.dir(profile);
+       dispalyData(profile)
+    });
 });
+}
+
+function dispalyData(data){
+    return console.log(`${data.name} has ${data.points.total} and ${data.badges.length}`);
+}
+username.forEach(user => getProfile(user));
+    
+ 
+
