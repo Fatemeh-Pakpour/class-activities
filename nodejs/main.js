d8871e2d483a0b9b56870e0c5043d2f1afdf50ec
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-09 15:58:41 +0200

    added handling status error to homework

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,27 +1,48 @@
+//  const users =["chalkers",  , ];
+function getProfile(username) {
+  const https = require("https");
+  const http = require("http");
+  const request = https.get(
+    `https://teamtreehouse.com/${username}.json`,
+    response => {
+      if (response.statusCode === 200) {
+        let responseBody = "";
+        response.on("data", data => {
+          responseBody += data.toString();
+        });
+        response.on("end", () => {
+          try {
+            const profile = JSON.parse(responseBody);
+            dispalyData(profile);
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } else {
+        const message = `There was an error for ${username}${
+          http.STATUS_CODES[response.statusCode]
+        }`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+    }
+  );
 
-const username =["chalkers", "alenaholligan","davemcfarland"];
-function getProfile(username){
-    const https = require("https");
-    https.get(`https://teamtreehouse.com/${username}.json`, response =>{
-    console.log('statusCode', response.statusCode);
-    let responseBody = "";
-    response.on('data', data =>{
-        responseBody += data.toString();
-    });
-    response.on('end', ()=>{
-        // console.log(typeof(responseBody));
-        // console.log(responseBody);
-        const profile = JSON.parse(responseBody);
-    //    console.dir(profile);
-       dispalyData(profile)
-    });
-});
+  request.on("error", error => {
+    console.error(`error is : ${error.message}`);
+  });
 }
-
-function dispalyData(data){
-    return console.log(`${data.name} has ${data.points.total} and ${data.badges.length}`);
+function printError(error) {
+  console.error(error.message);
 }
-username.forEach(user => getProfile(user));
-    
- 
+function dispalyData(data) {
+  return console.log(
+    `${data.name} has ${data.points.total} and ${data.badges.length}`
+  );
+}
+// users.forEach(username => getProfile(username));
+//when the function take one parameter and foreach passes one in we can shorten our code to :
 
+// there is another way to use global object process
+const users = process.argv.slice(2);
+users.forEach(getProfile);
